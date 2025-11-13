295224dc4ea3f00c6dab6e46aaf47f0c7eab2745
AbulkasimovBakhtiyar2006 <abulkasimovbahtiar3@gmail.com>
2025-11-13 15:56:25 +0500

    hardcoded token fix now we will send our roken to ai service

--- cv-pages/api.js
+++ cv-pages/api.js
@@ -356,18 +356,9 @@ class CVApiService {
      */
     async processDocumentWithAI(fileRef) {
         const AI_API_URL = 'http://157.230.103.104:4242/api/process-document';
-        const token = 'bWlzdGVyOjEyMzQ1Njc4';
-
-
-        // Create basic auth header
-        const basicAuth = token;
 
         return await this.makeRequest(AI_API_URL, {
             method: 'POST',
-            headers: {
-                'Authorization': `Basic ${basicAuth}`,
-                'Content-Type': 'application/json'
-            },
             body: JSON.stringify({
                 fileRef: fileRef
             })
